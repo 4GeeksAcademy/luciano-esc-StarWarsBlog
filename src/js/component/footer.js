e1650851aa175079c67170d0de505c5d898ee008
luciano-esc <149176025+luciano-esc@users.noreply.github.com>
2024-05-25 15:32:30 +0000

    blog terminado

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
+	<footer className="footer mt-5 py-3 text-center">
 		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+			Real web{" "}
+			<a href="https://www.starwars.com/databank">StarWars Databank</a>
 		</p>
 	</footer>
 );
--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -12,7 +12,8 @@ const getState = ({ getStore, getActions, setStore }) => {
 					background: "white",
 					initial: "white"
 				}
-			]
+			],
+			favorites: []
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
@@ -37,6 +38,18 @@ const getState = ({ getStore, getActions, setStore }) => {
 
 				//reset the global store
 				setStore({ demo: demo });
+			},
+
+			addToFavorites: item => {
+				const store = getStore();
+				const favorites = [...store.favorites, item];
+				setStore({ favorites: favorites });
+			},
+			
+			removeFromFavorites: index => {
+				const store = getStore();
+				const updatedFavorites = store.favorites.filter((item, i) => i !== index);
+				setStore({ favorites: updatedFavorites });
 			}
 		}
 	};
